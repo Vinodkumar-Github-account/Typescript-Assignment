8808d040b7b0d4bd22fc54ffe9527722ad6fd21d
Vinod Kumar <161883899+Vinodkumar-Github-account@users.noreply.github.com>
2024-03-03 01:21:18 +0500

    Add files via upload

---
+++ 14. Guest List.ts
@@ -0,0 +1,4 @@
+let guest_list: string [] = ['ali','raza','abdullah','aman','nawaz'];
+for(let i=0; i<guest_list.length; i++){
+    console.log('Respected Sir/Medam ' + guest_list[i] + ',\nwe invited you on dinner tomorrow.\n\nThank You\nFrom,\nVinod Kumar\n');
+}
